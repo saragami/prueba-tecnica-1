bfadce91c4f9eb08e7ed191522139dbd46386bb7
sara <saragaminop@gmail.com>
2023-12-16 20:48:23 -0600

    falta realizar cambios con metodo js y ordenamiento burbuja

---
+++ app.js
@@ -0,0 +1,23 @@
+// - Construye una función que reciba por parámetros un string y un array de strings. La función debe retornar un arreglo de strings con los strings que sean de mayor longitud que el string recibido por parámetros.
+
+palabra= "lo que sea"
+arreglo= ["insecto", "bootcamp", "escritorio"]
+
+
+function printArray( palabra, arreglo){
+    for ( i=0 ; i<=arreglo.length; i++){
+if( palabra.length=){
+
+}        
+
+    }
+
+
+
+
+
+};
+
+
+
+
